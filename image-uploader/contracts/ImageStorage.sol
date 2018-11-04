f31f0b68781b846ffae9c4511587594c67152cc4
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-11-04 11:35:05 -0200

    add smart contract

--- image-uploader/contracts/ImageStorage.sol
+++ image-uploader/contracts/ImageStorage.sol
@@ -1,14 +1,31 @@
 pragma solidity ^0.4.24;
 
 contract ImageStorage {
-  
-  string ipfsHash;
-
-  function saveImage(string _ipfsHash) public {
-    ipfsHash = _ipfsHash;
-  }
-
-  function getImage() public view returns (string) {
-    return ipfsHash;
-  }
+    
+    event newImage(uint id, address owner);
+    
+    struct Image {
+        address owner;
+        string ipfsHash;
+        uint allowedViewTimes;
+        uint viewdTimes;
+    }
+    
+    Image[] private images;
+    
+    function addImage(string _ipfsHash, uint _allowedViewTimes) public {
+        uint id = images.push(Image(msg.sender, _ipfsHash, _allowedViewTimes, 0)) - 1;
+        emit newImage(id, msg.sender);
+    }
+    
+    function getImage(uint _imageID) public returns (string, address){
+        
+        require(images[_imageID].viewdTimes < images[_imageID].allowedViewTimes, "You cannot see this image anymore");
+        images[_imageID].viewdTimes++;
+        return (
+            images[_imageID].ipfsHash,
+            images[_imageID].owner
+        );
+    }
+    
 }
