ff8f4feecd714e4860508cf9ec4869c9a0d0179c
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-11-10 15:53:19 -0200

    rm comments

--- image-uploader/client/src/App.js
+++ image-uploader/client/src/App.js
@@ -51,42 +51,6 @@ class App extends Component {
           />        
         </div>
       </div>
-      
-      // <div className="App">
-      //   <nav className="navbar pure-menu pure-menu-horizontal">
-      //     <a href="/" className="pure-menu-heading pure-menu-link">IPFS Image Upload DApp</a>
-      //   </nav>
-        
-      //   <main className="container">
-      //     <div className="pure-g">
-      //       <div className="pure-u-1-1">
-      //         <button onClick={this.getImage}>Your Image</button >
-      //         <p>This image is stored on IPFS & Private POA Blochchain</p>
-      //         <div style={{
-      //           width: '800px', 
-      //           height: '400px', 
-      //           border: '1px solid black',
-      //           alignSelf: 'center'}}
-      //         >
-      //           {
-      //             this.state.ipfsHash ? <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt=""/> : ''
-      //           }
-      //         </div>
-
-      //         <hr/>
-              
-      //         <h2>Upload Image</h2>
-              
-      //         <form onSubmit={this.onSubmit}>
-      //           <input type="file" onChange={this.captureFile} required/> <br/>
-      //           <input type="number" min="1" name="number" onChange={this.onChange} required/> <br/>
-      //           <input type="submit"/>
-      //         </form>
-              
-      //       </div>
-      //     </div>
-      //   </main>
-      // </div>
     );
   }
 }
